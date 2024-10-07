ee6c43b2dd95b86057c02a685e7a6eaa9809ffa9
anna-ayn <19-10096@usb.ve>
2024-10-07 17:41:29 -0400

    Reemplazar el archivo admin.js por el archivo compilado

--- src/navigation/admin.js
+++ src/navigation/admin.js
@@ -1,19 +1,21 @@
 'use strict';
 
-const validator = require('validator');
-const winston = require('winston');
+const __importDefault = (this && this.__importDefault) || function (mod) {
+	return (mod && mod.__esModule) ? mod : { default: mod };
+};
 
-const plugins = require('../plugins');
-const db = require('../database');
-const pubsub = require('../pubsub');
+Object.defineProperty(exports, '__esModule', { value: true });
+const validator_1 = __importDefault(require('validator'));
+const winston_1 = __importDefault(require('winston'));
+const plugins_1 = __importDefault(require('../plugins'));
+const database_1 = __importDefault(require('../database'));
+const pubsub_1 = __importDefault(require('../pubsub'));
 
-const admin = module.exports;
+const admin = {};
 let cache = null;
-
-pubsub.on('admin:navigation:save', () => {
+pubsub_1.default.on('admin:navigation:save', () => {
 	cache = null;
 });
-
 admin.save = async function (data) {
 	const order = Object.keys(data);
 	const bulkSet = [];
@@ -24,54 +26,50 @@ admin.save = async function (data) {
 		}
 		bulkSet.push([`navigation:enabled:${item.order}`, item]);
 	});
-
 	cache = null;
-	pubsub.publish('admin:navigation:save');
-	const ids = await db.getSortedSetRange('navigation:enabled', 0, -1);
-	await db.deleteAll(ids.map(id => `navigation:enabled:${id}`));
-	await db.setObjectBulk(bulkSet);
-	await db.delete('navigation:enabled');
-	await db.sortedSetAdd('navigation:enabled', order, order);
+	pubsub_1.default.publish('admin:navigation:save');
+	const ids = await database_1.default.getSortedSetRange('navigation:enabled', 0, -1);
+	await database_1.default.deleteAll(ids.map(id => `navigation:enabled:${id}`));
+	await database_1.default.setObjectBulk(bulkSet);
+	await database_1.default.delete('navigation:enabled');
+	await database_1.default.sortedSetAdd('navigation:enabled', order, order);
 };
-
 admin.getAdmin = async function () {
 	const [enabled, available] = await Promise.all([
 		admin.get(),
 		getAvailable(),
 	]);
-	return { enabled: enabled, available: available };
+	return { enabled, available };
 };
-
 const fieldsToEscape = ['iconClass', 'class', 'route', 'id', 'text', 'textClass', 'title'];
-
 admin.escapeFields = navItems => toggleEscape(navItems, true);
 admin.unescapeFields = navItems => toggleEscape(navItems, false);
-
 function toggleEscape(navItems, flag) {
 	navItems.forEach((item) => {
 		if (item) {
 			fieldsToEscape.forEach((field) => {
 				if (item.hasOwnProperty(field)) {
-					item[field] = validator[flag ? 'escape' : 'unescape'](String(item[field]));
+					item[field] = validator_1.default[flag ? 'escape' : 'unescape'](String(item[field]));
 				}
 			});
 		}
 	});
 }
-
 admin.get = async function () {
 	if (cache) {
 		return cache.map(item => ({ ...item }));
 	}
-	const ids = await db.getSortedSetRange('navigation:enabled', 0, -1);
-	const data = await db.getObjects(ids.map(id => `navigation:enabled:${id}`));
+	const ids = await database_1.default.getSortedSetRange('navigation:enabled', 0, -1);
+	const data = await database_1.default.getObjects(ids.map(id => `navigation:enabled:${id}`));
 	cache = data.filter(Boolean).map((item) => {
 		if (item.hasOwnProperty('groups')) {
 			try {
 				item.groups = JSON.parse(item.groups);
 			} catch (err) {
-				winston.error(err.stack);
-				item.groups = [];
+				if (err instanceof Error) {
+					winston_1.default.error(err.stack);
+					item.groups = [];
+				}
 			}
 		}
 		item.groups = item.groups || [];
@@ -81,18 +79,15 @@ admin.get = async function () {
 		return item;
 	});
 	admin.escapeFields(cache);
-
 	return cache.map(item => ({ ...item }));
 };
-
 async function getAvailable() {
 	const core = require('../../install/data/navigation.json').map((item) => {
 		item.core = true;
 		item.id = item.id || '';
 		return item;
 	});
-
-	const navItems = await plugins.hooks.fire('filter:navigation.available', core);
+	const navItems = await plugins_1.default.hooks.fire('filter:navigation.available', core);
 	navItems.forEach((item) => {
 		if (item && !item.hasOwnProperty('enabled')) {
 			item.enabled = true;
@@ -100,5 +95,6 @@ async function getAvailable() {
 	});
 	return navItems;
 }
-
 require('../promisify')(admin);
+
+exports.default = admin;
