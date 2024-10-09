f8acbdf4a53fab7aaca88f2c3d61a1ee35c0b901
anna-ayn <19-10096@usb.ve>
2024-10-09 18:13:10 -0400

    Resolver problemas de linter en el archivo TS y agregar el archivo compilado

--- src/navigation/admin.ts
+++ src/navigation/admin.ts
@@ -1,9 +1,9 @@
-'use strict';
+
 
 import validator from 'validator';
 import winston from 'winston';
 
-// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call 
+// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
 import plugins from '../plugins';
 
 // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
@@ -12,6 +12,8 @@ import db from '../database';
 // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
 import pubsub from '../pubsub';
 
+// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+import promisify from '../promisify';
 
 // Interface for the structure of navigation items
 interface NavigationItem {
@@ -42,101 +44,100 @@ const admin: Admin = {} as Admin;
 let cache: NavigationItem[] | null = null;
 
 pubsub.on('admin:navigation:save', () => {
-    cache = null;
+	cache = null;
 });
 
 admin.save = async function (data: NavigationItem[]): Promise<void> {
-    const order: string[] = Object.keys(data);
-    const bulkSet: [string, NavigationItem][] = [];
-    data.forEach((item, index) => {
-        item.order = order[index];
-        if (item.hasOwnProperty('groups')) {
-            item.groups = JSON.stringify(item.groups);
-        }
-        bulkSet.push([`navigation:enabled:${item.order}`, item]);
-    });
+	const order: string[] = Object.keys(data);
+	const bulkSet: [string, NavigationItem][] = [];
+	data.forEach((item, index) => {
+		item.order = order[index];
+		if (item.hasOwnProperty('groups')) {
+			item.groups = JSON.stringify(item.groups);
+		}
+		bulkSet.push([`navigation:enabled:${item.order}`, item]);
+	});
 
-    cache = null;
-    pubsub.publish('admin:navigation:save');
-    const ids: string[] = await db.getSortedSetRange('navigation:enabled', 0, -1);
-    await db.deleteAll(ids.map(id => `navigation:enabled:${id}`));
-    await db.setObjectBulk(bulkSet);
-    await db.delete('navigation:enabled');
-    await db.sortedSetAdd('navigation:enabled', order, order);
+	cache = null;
+	pubsub.publish('admin:navigation:save');
+	const ids: string[] = await db.getSortedSetRange('navigation:enabled', 0, -1);
+	await db.deleteAll(ids.map(id => `navigation:enabled:${id}`));
+	await db.setObjectBulk(bulkSet);
+	await db.delete('navigation:enabled');
+	await db.sortedSetAdd('navigation:enabled', order, order);
 };
 
+async function getAvailable(): Promise<NavigationItem[]> {
+	const core: NavigationItem[] = require('../../install/data/navigation.json').map((item: NavigationItem) => {
+		item.core = true;
+		item.id = item.id || '';
+		return item;
+	});
+
+	const navItems: NavigationItem[] = await plugins.hooks.fire('filter:navigation.available', core);
+	navItems.forEach((item) => {
+		if (item && !item.hasOwnProperty('enabled')) {
+			item.enabled = true;
+		}
+	});
+	return navItems;
+}
+
 admin.getAdmin = async function (): Promise<{ enabled: NavigationItem[], available: NavigationItem[] }> {
-    const [enabled, available] = await Promise.all([
-        admin.get(),
-        getAvailable(),
-    ]);
-    return { enabled: enabled, available: available };
+	const [enabled, available] = await Promise.all([
+		admin.get(),
+		getAvailable(),
+	]);
+	return { enabled: enabled, available: available };
 };
 
 const fieldsToEscape: (keyof NavigationItem)[] = ['iconClass', 'class', 'route', 'id', 'text', 'textClass', 'title'];
 
+function toggleEscape(navItems: NavigationItem[], flag: boolean): void {
+	navItems.forEach((item) => {
+		if (item) {
+			fieldsToEscape.forEach((field) => {
+				if (item.hasOwnProperty(field)) {
+					item[field] = validator[flag ? 'escape' : 'unescape'](String(item[field]));
+				}
+			});
+		}
+	});
+}
+
 admin.escapeFields = (navItems: NavigationItem[]): void => toggleEscape(navItems, true);
 admin.unescapeFields = (navItems: NavigationItem[]): void => toggleEscape(navItems, false);
 
-function toggleEscape(navItems: NavigationItem[], flag: boolean): void {
-    navItems.forEach((item) => {
-        if (item) {
-            fieldsToEscape.forEach((field) => {
-                if (item.hasOwnProperty(field)) {
-                    item[field] = validator[flag ? 'escape' : 'unescape'](String(item[field]));
-                }
-            });
-        }
-    });
-}
-
 admin.get = async function (): Promise<NavigationItem[]> {
-    if (cache) {
-        return cache.map(item => ({ ...item }));
-    }
-    const ids: string[] = await db.getSortedSetRange('navigation:enabled', 0, -1);
-    const data: NavigationItem[] = await db.getObjects(ids.map(id => `navigation:enabled:${id}`));
-    cache = data.filter(Boolean).map((item) => {
-        if (item.hasOwnProperty('groups')) {
-            try {
-                item.groups = JSON.parse(item.groups as string);
-            } catch (err) {
-                if (err instanceof Error) {
-                    winston.error(err.stack);
-                } else {
-                    winston.error('Unknown error', err);
-                }
-                item.groups = [];
-            }
-        }
-        item.groups = item.groups || [];
-        if (item.groups && !Array.isArray(item.groups)) {
-            item.groups = [item.groups];
-        }
-        return item;
-    });
-    admin.escapeFields(cache);
+	if (cache) {
+		return cache.map(item => ({ ...item }));
+	}
+	const ids: string[] = await db.getSortedSetRange('navigation:enabled', 0, -1);
+	const data: NavigationItem[] = await db.getObjects(ids.map(id => `navigation:enabled:${id}`));
+	cache = data.filter(Boolean).map((item) => {
+		if (item.hasOwnProperty('groups')) {
+			try {
+				item.groups = JSON.parse(item.groups as string);
+			} catch (err) {
+				if (err instanceof Error) {
+					winston.error(err.stack);
+				} else {
+					winston.error('Unknown error', err);
+				}
+				item.groups = [];
+			}
+		}
+		item.groups = item.groups || [];
+		if (item.groups && !Array.isArray(item.groups)) {
+			item.groups = [item.groups];
+		}
+		return item;
+	});
+	admin.escapeFields(cache);
 
-    return cache.map(item => ({ ...item }));
+	return cache.map(item => ({ ...item }));
 };
 
-async function getAvailable(): Promise<NavigationItem[]> {
-    const core: NavigationItem[] = require('../../install/data/navigation.json').map((item: NavigationItem) => {
-        item.core = true;
-        item.id = item.id || '';
-        return item;
-    });
-
-    const navItems: NavigationItem[] = await plugins.hooks.fire('filter:navigation.available', core);
-    navItems.forEach((item) => {
-        if (item && !item.hasOwnProperty('enabled')) {
-            item.enabled = true;
-        }
-    });
-    return navItems;
-}
-
-require('../promisify')(admin);
-module.exports = admin;
-
+promisify(admin);
+export default admin;
 
