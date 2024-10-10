03f022fb4985289372c83dcceb94018b0f87428c
anna-ayn <19-10096@usb.ve>
2024-10-09 23:04:03 -0400

    Reparar error de admin.get no es una funcion

--- src/navigation/admin.js
+++ src/navigation/admin.js
@@ -19,64 +19,11 @@ const database_1 = __importDefault(require("../database"));
 const pubsub_1 = __importDefault(require("../pubsub"));
 const promisify_1 = __importDefault(require("../promisify"));
 const navigation_json_1 = __importDefault(require("../../install/data/navigation.json"));
-const admin = {};
 let cache = null;
 // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
 pubsub_1.default.on('admin:navigation:save', () => {
     cache = null;
 });
-admin.save = function (data) {
-    return __awaiter(this, void 0, void 0, function* () {
-        const order = Object.keys(data);
-        const bulkSet = [];
-        data.forEach((item, index) => {
-            item.order = order[index];
-            if (item.hasOwnProperty('groups')) {
-                item.groups = JSON.stringify(item.groups);
-            }
-            bulkSet.push([`navigation:enabled:${item.order}`, item]);
-        });
-        cache = null;
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-        pubsub_1.default.publish('admin:navigation:save');
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-        const ids = yield database_1.default.getSortedSetRange('navigation:enabled', 0, -1);
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-        yield database_1.default.deleteAll(ids.map(id => `navigation:enabled:${id}`));
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-        yield database_1.default.setObjectBulk(bulkSet);
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-        yield database_1.default.delete('navigation:enabled');
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-        yield database_1.default.sortedSetAdd('navigation:enabled', order, order);
-    });
-};
-function getAvailable() {
-    return __awaiter(this, void 0, void 0, function* () {
-        const core = navigation_json_1.default.map((item) => {
-            item.core = true;
-            item.id = item.id || '';
-            return item;
-        });
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-        const navItems = yield plugins_1.default.hooks.fire('filter:navigation.available', core);
-        navItems.forEach((item) => {
-            if (item && !item.hasOwnProperty('enabled')) {
-                item.enabled = true;
-            }
-        });
-        return navItems;
-    });
-}
-admin.getAdmin = function () {
-    return __awaiter(this, void 0, void 0, function* () {
-        const [enabled, available] = yield Promise.all([
-            admin.get(),
-            getAvailable(),
-        ]);
-        return { enabled: enabled, available: available };
-    });
-};
 const fieldsToEscape = ['iconClass', 'class', 'route', 'id', 'text', 'textClass', 'title'];
 function toggleEscape(navItems, flag) {
     navItems.forEach((item) => {
@@ -89,41 +36,95 @@ function toggleEscape(navItems, flag) {
         }
     });
 }
-admin.escapeFields = (navItems) => toggleEscape(navItems, true);
-admin.unescapeFields = (navItems) => toggleEscape(navItems, false);
-admin.get = function () {
-    return __awaiter(this, void 0, void 0, function* () {
-        if (cache) {
-            return cache.map(item => (Object.assign({}, item)));
-        }
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-        const ids = yield database_1.default.getSortedSetRange('navigation:enabled', 0, -1);
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-        const data = yield database_1.default.getObjects(ids.map(id => `navigation:enabled:${id}`));
-        cache = data.filter(Boolean).map((item) => {
-            if (item.hasOwnProperty('groups')) {
-                try {
-                    item.groups = JSON.parse(item.groups);
+const admin = {
+    save: function (data) {
+        return __awaiter(this, void 0, void 0, function* () {
+            const order = Object.keys(data);
+            const bulkSet = [];
+            data.forEach((item, index) => {
+                item.order = order[index];
+                if (item.hasOwnProperty('groups')) {
+                    item.groups = JSON.stringify(item.groups);
                 }
-                catch (err) {
-                    if (err instanceof Error) {
-                        winston_1.default.error(err.stack);
-                    }
-                    else {
-                        winston_1.default.error('Unknown error', err);
-                    }
-                    item.groups = [];
-                }
-            }
-            item.groups = item.groups || [];
-            if (item.groups && !Array.isArray(item.groups)) {
-                item.groups = [item.groups];
-            }
-            return item;
+                bulkSet.push([`navigation:enabled:${item.order}`, item]);
+            });
+            cache = null;
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+            pubsub_1.default.publish('admin:navigation:save');
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+            const ids = yield database_1.default.getSortedSetRange('navigation:enabled', 0, -1);
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+            yield database_1.default.deleteAll(ids.map(id => `navigation:enabled:${id}`));
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+            yield database_1.default.setObjectBulk(bulkSet);
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+            yield database_1.default.delete('navigation:enabled');
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+            yield database_1.default.sortedSetAdd('navigation:enabled', order, order);
         });
-        admin.escapeFields(cache);
-        return cache.map(item => (Object.assign({}, item)));
-    });
+    },
+    get: function () {
+        return __awaiter(this, void 0, void 0, function* () {
+            if (cache) {
+                return cache.map(item => (Object.assign({}, item)));
+            }
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+            const ids = yield database_1.default.getSortedSetRange('navigation:enabled', 0, -1);
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+            const data = yield database_1.default.getObjects(ids.map(id => `navigation:enabled:${id}`));
+            cache = data.filter(Boolean).map((item) => {
+                if (item.hasOwnProperty('groups')) {
+                    try {
+                        item.groups = JSON.parse(item.groups);
+                    }
+                    catch (err) {
+                        if (err instanceof Error) {
+                            winston_1.default.error(err.stack);
+                        }
+                        else {
+                            winston_1.default.error('Unknown error', err);
+                        }
+                        item.groups = [];
+                    }
+                }
+                item.groups = item.groups || [];
+                if (item.groups && !Array.isArray(item.groups)) {
+                    item.groups = [item.groups];
+                }
+                return item;
+            });
+            admin.escapeFields(cache);
+            return cache.map(item => (Object.assign({}, item)));
+        });
+    },
+    getAvailable: function () {
+        return __awaiter(this, void 0, void 0, function* () {
+            const core = navigation_json_1.default.map((item) => {
+                item.core = true;
+                item.id = item.id || '';
+                return item;
+            });
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+            const navItems = yield plugins_1.default.hooks.fire('filter:navigation.available', core);
+            navItems.forEach((item) => {
+                if (item && !item.hasOwnProperty('enabled')) {
+                    item.enabled = true;
+                }
+            });
+            return navItems;
+        });
+    },
+    getAdmin: function () {
+        return __awaiter(this, void 0, void 0, function* () {
+            const [enabled, available] = yield Promise.all([
+                admin.get(),
+                admin.getAvailable(),
+            ]);
+            return { enabled: enabled, available: available };
+        });
+    },
+    escapeFields: (navItems) => toggleEscape(navItems, true),
+    unescapeFields: (navItems) => toggleEscape(navItems, false),
 };
 (0, promisify_1.default)(admin);
 exports.default = admin;
